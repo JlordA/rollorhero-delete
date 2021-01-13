c79d598a3d4d6c34bafac8febbec173028c377e6
JlordA <james.ardery@gmail.com>
2021-01-13 11:27:16 -0500

    first commit with google maps api test

--- src/Header.js
+++ src/Header.js
@@ -19,9 +19,3 @@ const HeaderExampleImage = () => (
 
 export default HeaderExampleImage
 
-{/* <iframe
-            width="600"
-            height="450"
-            frameborder="0" 
-            src="https://www.google.com/maps/embed/v1/search?zoom=16&key=AIzaSyCtcZhnuZsw60rHQctphAItzEqaNhwi_Dg&q=Emilys+Pork+Store+Williamsburg+Brooklyn" allowfullscreen>
-        </iframe> */}
